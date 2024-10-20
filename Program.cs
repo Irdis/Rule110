0e95f2b51cfb807b21321ae266c3114218ec9695
Ivan Novitskii <ivannovitskii@gmail.com>
2024-10-21 00:45:10 +0100

    bhn glider for 1

--- Program.cs
+++ Program.cs
@@ -5,26 +5,20 @@ class Program
     static void Main(string[] args)
     {
         var rand = new Random();
-        for (int k = 0; k < 15; k++)
+        var scene = new Scene(1000);
+
+        var gliders = new List<(int, IGlider)>();
+        gliders.Add((4, new ANGlider(5)));
+        gliders.Add((40, new BHNGlider()));
+
+        scene.FillWithEther(gliders);
+        scene.Draw();
+
+        for (int j = 0; j < scene.Size; j++)
         {
-            var scene = new Scene(2500, $"img{k}.bmp");
-
-            var gliders = new List<(int, IGlider)>();
-            gliders.Add((1, new ANGlider(15)));
-            for (int i = 0; i < k; i++)
-            {
-                gliders.Add((15 + i, new BGlider()));
-            }
-
-            scene.FillWithEther(gliders);
+            scene.Next();
             scene.Draw();
-
-            for (int j = 0; j < scene.Size; j++)
-            {
-                scene.Next();
-                scene.Draw();
-            }
-            scene.SaveImg();
         }
+        scene.SaveImg();
     }
 }
