7a4c84d5cf41ca01b06ef4339ce3166f1543e1dc
Ivan Novitskii <ivannovitskii@gmail.com>
2025-04-11 22:24:35 +0100

    spelling

--- Program.cs
+++ Program.cs
@@ -35,11 +35,11 @@ public class Program
             "B 13A B 11A B 12A B C F G",
         ];
         var a4GliderCollection = new ANGliderCollection(3);
-        var ehGliderCollecion = new EHatGliderCollection();
-        var en1GliderCollecion = new ENGliderCollection(0);
-        var en2GliderCollecion = new ENGliderCollection(1);
-        var en4GliderCollecion = new ENGliderCollection(3);
-        var en5GliderCollecion = new ENGliderCollection(4);
+        var ehGliderCollection = new EHatGliderCollection();
+        var en1GliderCollection = new ENGliderCollection(0);
+        var en2GliderCollection = new ENGliderCollection(1);
+        var en4GliderCollection = new ENGliderCollection(3);
+        var en5GliderCollection = new ENGliderCollection(4);
 
         for (int i = 0; i < patterns.Length; i++)
         /*for (int i = 0; i < 30; i++)*/
@@ -48,11 +48,11 @@ public class Program
             /*var pattern = patterns[0];*/
             var encoder = new BlockEncoder(
                     a4GliderCollection,
-                    ehGliderCollecion,
-                    en1GliderCollecion,
-                    en2GliderCollecion,
-                    en4GliderCollecion,
-                    en5GliderCollecion,
+                    ehGliderCollection,
+                    en1GliderCollection,
+                    en2GliderCollection,
+                    en4GliderCollection,
+                    en5GliderCollection,
                     i
             );
 
