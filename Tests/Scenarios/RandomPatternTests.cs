cbcf1644d90043b2c62b4f62dc2bc81bf63a7e24
Ivan Navitski <ivannovitskii@gmail.com>
2026-06-20 21:49:10 +0100

    random

--- Tests/Scenarios/RandomPatternTests.cs
+++ Tests/Scenarios/RandomPatternTests.cs
@@ -5,50 +5,7 @@ namespace Rule110.Tests.Scenarios;
 [Tag("random")]
 public class RandomPatternTests : Rule110TestBase
 {
-    [TestCase(1, "rand110", 110, 300)]
-    [TestCase(1, "rand109", 109, 300)]
-    [TestCase(1, "rand30", 30, 300)]
-    [TestCase(1, "rand90", 90, 300)]
-    [TestCase(1, "rand184", 184, 300)]
-    public void GenerateRandomPattern(int prefNum, string prefStr, int ruleNumber, int size)
-    {
-        var random = new Random();
-        for (int i = 0; i < 3; i++)
-        {
-            var background = new EmptyBackground();
-            var actualImgName = GetImgActualPath(prefNum, prefStr, FormatNumber(i));
-            var baselineImgName = GetImgBaselinePath(prefNum, prefStr, FormatNumber(i));
-
-            var observers = new List<IObserver>
-            {
-                new ImgObserver(size, actualImgName),
-            };
-            var scene = new Scene(size, background, observers, ruleNumber);
-
-            var gliders = new List<(int, IGlider)>();
-            scene.Init(gliders);
-
-            for(int j = 0; j < size; j++)
-            {
-                scene.SetState(j, random.Next(2));
-            }
-
-            scene.InitComplete();
-
-            for (int j = 1; j < scene.Size; j++)
-            {
-                scene.Next();
-            }
-            scene.Complete();
-
-            Assert.True(File.Exists(actualImgName));
-
-            // File.Copy(actualImgName, baselineImgName, overwrite: true);
-        }
-    }
-
-    // 18, 22, 26, 30, 45, 54, 57, 60, 62, 73, 75, 90, 101, 105, 106, 122, 126
-    // [TestCase(2, "all", 300)]
+    [TestCase(1, "all", 300)]
     public void GenerateAllNumbers(int prefNum, string prefStr, int size)
     {
         var random = new Random();
@@ -60,7 +17,7 @@ public class RandomPatternTests : Rule110TestBase
 
             var observers = new List<IObserver>
             {
-                new ImgObserver(size, actualImgName, blockSize: 3),
+                new ImgObserver(size, actualImgName),
             };
             var scene = new Scene(size, background, observers, i);
 
@@ -81,6 +38,7 @@ public class RandomPatternTests : Rule110TestBase
             scene.Complete();
 
             Assert.True(File.Exists(actualImgName));
+            // File.Copy(actualImgName, baselineImgName, overwrite: true);
         }
     }
 }
