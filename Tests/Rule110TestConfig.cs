ac34bb9ff4d3f79be32b9bc11f21f881ff0060ca
Ivan Navitski <ivannovitskii@gmail.com>
2026-06-21 01:02:06 +0100

    recording

--- Tests/Rule110TestConfig.cs
+++ Tests/Rule110TestConfig.cs
@@ -5,12 +5,12 @@ namespace Rule110.Tests;
 public class Rule110TestConfig
 {
     public TestMode Mode { 
-        get => Recording == "y" 
+        get => Recording
             ? TestMode.Recording 
             : TestMode.Testing; 
     }
 
-    public string Recording { get; set; }
+    public bool Recording { get; set; }
 
     public bool WriteToGallery { get; set; }
 }
